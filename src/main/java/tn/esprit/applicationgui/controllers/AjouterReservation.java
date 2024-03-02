754f90bab37d4e64855639d5520988551d2cd0d9
Amira <amira.laffet@esprit.tn>
2024-03-02 10:52:53 +0100

    sans tri et recherche

--- src/main/java/tn/esprit/applicationgui/controllers/AjouterReservation.java
+++ src/main/java/tn/esprit/applicationgui/controllers/AjouterReservation.java
@@ -151,4 +151,10 @@ public class AjouterReservation {
         alert.setContentText(content);
         alert.showAndWait();
     }
+    /*private void showAlert(Alert.AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();*/
+    }
 }
